57a1a1b8de0d0bea710503cd369a5e0b9eb5cb1b
thengansax <thengansax612@gmail.com>
2022-05-09 14:29:27 +0700

    d

---
+++ app/scripts/main.js
@@ -0,0 +1,8 @@
+$('.carousel').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: '<div class="slick-prev"><i class="fa-solid fa-angle-right"></i></div>',
+    nextArrow: '<div class="slick-next"><i class="fa-solid fa-chevron-left"></i></div>',
+    centerMode: true,
+  });
